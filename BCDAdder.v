adf28bb2eced66feabc8479d06e03e7a9fec7b7e
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-02 07:50:05 -0500

    changed declarations and assignment

--- BCDAdder.v
+++ BCDAdder.v
@@ -2,17 +2,17 @@
 
 module BCDAdder(
     input[3:0] A,B,
-    output S3,S2,S1,S0
+    output S3,S2,S1,S0,Co
     );
     
     wire S_3,S_2,S_1,S_0,C_int, OR_Result;
-    reg[3:0] newB;
-    reg Co;
+    wire[3:0] newB;
+    wire Co;
     
     fourBitAdder f1(A,B,1'b0,S_0,S_1,S_2,S_3,C_int);
     
     assign OR_Result = C_int | S_3&S_2 | S_3&S_1;
-    assign newB = {1'b0,OR_Result,OR_Result,1'b0}
+    assign newB = {1'b0,OR_Result,OR_Result,1'b0};
     
     fourBitAdder f2({S_3,S_2,S_1,S_0},newB,1'b0,S0,S1,S2,S3,Co);
     
