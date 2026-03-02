38585119f5fdeb8c306b4f8817a852f88a5ae7a1
kadgitub7 <kadhir.ponnambalam@gmail.com>
2026-03-02 07:32:50 -0500

    make sure 4 bit results is given

--- BCDAdder.v
+++ BCDAdder.v
@@ -12,7 +12,7 @@ module BCDAdder_tb(
     fourBitAdder f1(A,B,1'b0,S_0,S_1,S_2,S_3,C_int);
     
     assign OR_Result = C_int | S_3&S_2 | S_3&S_1;
-    assign newB = {0,OR_Result,OR_Result,0};
+    assign newB = {1'b0,OR_Result,OR_Result,1'b0}
     
     fourBitAdder f2({S_3,S_2,S_1,S_0},newB,1'b0,S0,S1,S2,S3,Co);
     
